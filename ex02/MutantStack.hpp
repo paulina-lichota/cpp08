4862384a3726f84be779bd60a17f9c1963980984
paulich07 <50242426+paulich07@users.noreply.github.com>
2026-03-10 16:55:51 +0100

    wip

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,14 +6,29 @@
 /*   By: plichota <plichota@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 16:42:32 by plichota          #+#    #+#             */
-/*   Updated: 2026/03/10 16:42:40 by plichota         ###   ########.fr       */
+/*   Updated: 2026/03/10 16:55:36 by plichota         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+    stack e' un container adapter
+    sotto usa deque (oppure vector o list se specificato), accessibile con protected c
+*/
 
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
+#include <stack>
 
+template <class T>
+class MutantStack 
+{
+    private:
+        
+    public:
+        
+};
 
+#include "MutantStack.tpp"
 
 #endif
